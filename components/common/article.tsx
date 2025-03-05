cea74e8323d37e38c82ebd93e135c03c8791c3b0
Lupin <lupin.nguyen@outlook.com>
2025-03-05 11:29:50 +0700

    feat: enhance article component to support links and Google Drive embeds; update image handling

--- components/common/article.tsx
+++ components/common/article.tsx
@@ -13,6 +13,7 @@ import {
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
 
 const components: Options['components'] = {
   h1: ({ children, ...rest }) => <TypographyH1 {...rest}>{children}</TypographyH1>,
@@ -22,18 +23,50 @@ const components: Options['components'] = {
   p: ({ node, children, ...rest }) => {
     const el = node?.children[0];
 
-    if (el?.type === 'element' && el.tagName === 'img') {
-      return (
-        <div className="mt-3 flex justify-center" {...rest}>
-          <Image
-            className="rounded-xs shadow"
-            src={`${el.properties.src}`}
-            alt={`${el.properties.alt}`}
-            width="600"
-            height="300"
-          />
-        </div>
-      );
+    if (el?.type === 'element') {
+      switch (el.tagName) {
+        case 'img':
+          return (
+            <div className="mt-3 flex w-full justify-center" {...rest}>
+              <Image
+                className="rounded-xs shadow"
+                src={`${el.properties.src}`}
+                alt={`${el.properties.alt}`}
+                width="768"
+                height="768"
+              />
+            </div>
+          );
+        case 'a': {
+          const href = el.properties.href as string;
+
+          const googleDriveMatch = RegExp(/drive\.google\.com\/file\/d\/([^/]+)\//).exec(href);
+
+          if (googleDriveMatch) {
+            const id = googleDriveMatch[1];
+
+            return (
+              <div className="mt-3 flex w-full justify-center" {...rest}>
+                <iframe
+                  title={id}
+                  className="rounded-md shadow"
+                  width="768"
+                  height="432"
+                  src={`https://drive.google.com/file/d/${id}/preview`}
+                  allow="autoplay"
+                  allowFullScreen
+                />
+              </div>
+            );
+          }
+
+          return (
+            <div className="mt-3 flex w-full justify-center" {...rest}>
+              <Link href={href}>{children}</Link>
+            </div>
+          );
+        }
+      }
     }
     return <TypographyP {...rest}>{children}</TypographyP>;
   },
